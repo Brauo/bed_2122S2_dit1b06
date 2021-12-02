adcce65d790c21189c6bdceb27eddbc7c8d26a67
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2021-12-02 11:40:48 +0800

    chap 6 complete

--- myFirstWS/controller/app.js
+++ myFirstWS/controller/app.js
@@ -1,12 +1,55 @@
 var express = require('express');
 var app = express();
 var user = require('../model/user.js'); 
+//-- pg 21 chapter 6
+// import categoryDB from category.js, name as category
+var category = require('../model/category.js');
+//-- pg 22 chapter 6
+// import furnitureDB from furniture.js
+var furniture = require('../model/furniture.js');
 
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.json());// parse application/json
 app.use(urlencodedParser); // parse application/x-www-form-urlencoded
+
+// pg 22 chapter 6. get furniture by category
+app.get('/api/category/:catid/furniture', function (req, res) {
+    //fill in your code
+    // get cat id
+    var catid = req.params.catid
+
+    // call the getFurnitureByCat
+    furniture.getFurnitureByCat(catid, (err, result)=>{ // arrow function
+        if(err){ // if there is error
+            // assume return status code 500
+            res.status(500).send({"message":"some error"})
+        }else{ // no error
+            // assume respond status code 200
+            // just respond with the result
+            res.status(200).send(result)
+        }
+    })
+ 
+ });
+ 
+
+// pg 21 chapter 6. get the category
+app.get('/api/category', function (req, res) {
+    // use the object categoryDB in category.js
+    // call getCategory function or method in object
+    category.getCategory( function (err, result) { //-- callback function
+        if (!err) {
+            res.send(result);
+        }
+        else{
+            console.log(err);
+            res.status(500).send("Some error");
+         }
+    });
+});
+
 
 // add the DELETE api
 app.delete('/api/user/:userid', function (req, res) {
